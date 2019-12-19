95a29753ac3aff8ff7792d27639d6e22f0b0fc20
Johan van den Dorpe <johan.vandendorpe@sohonet.com>
2019-12-19 18:50:51 +0000

    honour calendar_months parameter in get_listing_calendar_

--- airbnb/api.py
+++ airbnb/api.py
@@ -236,8 +236,9 @@ class Api(object):
         }
 
         starting_date_str = starting_date.strftime("%Y-%m-%d")
+        end_days = calendar_months * 30
         ending_date_str = (
-            starting_date + datetime.timedelta(days=30)).strftime("%Y-%m-%d")
+            starting_date + datetime.timedelta(days=end_days)).strftime("%Y-%m-%d")
 
         r = self._session.get(API_URL + "/calendars/{}/{}/{}".format(
             str(listing_id), starting_date_str, ending_date_str), params=params)
